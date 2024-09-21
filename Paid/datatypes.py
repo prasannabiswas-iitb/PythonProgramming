7d112bcbfe0149e8804d3a9af3e8b0d372896bda
Prasanna-Biswas <prasanna.biswas14@gmail.com>
2024-09-21 15:04:56 +0530

    Sample test

---
+++ Paid/datatypes.py
@@ -0,0 +1,33 @@
+'''
+int
+float
+string
+boolean
+complex
+'''
+#immutable
+'''
+list
+tuple -> #immutable
+dict
+set
+'''
+#Variable
+'''
+A-Z, a-z, 0-9, _
+1. cannot start with numbers
+2. case-sensitive
+3. self-explanatory
+'''
+
+# Comparision operators
+# == != < > <= >=
+
+# Arithmetic operators
+# + - / * % // **
+
+# Logical operators
+# and or not
+x = 5
+y = "hello"
+print(not (x == 5 or y == "hEllo"))
